2cce702f46baa2fafb142159871dc00e8fe4d7e2
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 10:17:57 -0400

    Update delimiter-regex.js

--- js/delimiter-regex.js
+++ js/delimiter-regex.js
@@ -3,8 +3,11 @@
     }
     
     const delimit = (str, res) => {
+         var date1 = new Date();
          res.innerHTML = 
          str.replace(/( )(?=(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|II|III|IV|V|JR|SR|ESQ|DE|LA|LE|VAN|DER|DEL|DELLA|E|VON|AL|BEN|BIN|DA|DAS|DEN|DI|DO|DOS|D|DU|EL|IL|L|MAC|O|SAINT|SAINTE|ST|STE|TEN|TER|V|VD|SEN|ZUM)( |\.))/ig, "900001VARHOLDER")
             .replace(/((?:[^ ]* ){1}[^ ]*) /ig, '<tr><td>'+'$1'+'<br></td></tr>')
             .replace(/900001VARHOLDER/g, " ");
+         var date2 = new Date();
+         var diff = date2 - date1; console.log(diff)
     }
