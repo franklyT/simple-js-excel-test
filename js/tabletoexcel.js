b9643a5bebaa4079940c3e77dd1447b439b7c856
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 09:14:22 -0400

    Update tabletoexcel.js

--- js/tabletoexcel.js
+++ js/tabletoexcel.js
@@ -6,6 +6,9 @@
   return function(table, name) {
     if (!table.nodeType) table = document.getElementById(table)
     var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-    window.location.href = uri + base64(format(template, ctx))
+    var link = document.createElement("a");
+    link.download = "table.xls";
+    link.href = uri + base64(format(template, ctx));
+    link.click();
   }
 })()
