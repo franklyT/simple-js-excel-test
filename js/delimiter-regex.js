1ba13ef3e7e9a1db0082865f11c8df3e4120554a
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 08:59:33 -0400

    Update delimiter-regex.js

--- js/delimiter-regex.js
+++ js/delimiter-regex.js
@@ -3,12 +3,11 @@
         res.innerHTML = "(click 'Process')"; 
     }
     
-    const delimit = (str, res, table) => {
+    const delimit = (str, res) => {
             const x = 
          str.replace(/( )(?=(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|II|III|IV|V|JR|SR|ESQ|DE|LA|LE|VAN|DER|DEL|DELLA|E|VON|AL|BEN|BIN|DA|DAS|DEN|DI|DO|DOS|D|DU|EL|IL|L|MAC|O|SAINT|SAINTE|ST|STE|TEN|TER|V|VD|SEN|ZUM)( |\.))/ig, "900001VARHOLDER")
             .replace(/((?:[^ ]* ){1}[^ ]*) /ig, '<tr><td>'+'$1'+'<br></td></tr>')
             .replace(/900001VARHOLDER/g, " ");
       
-            res.innerHTML = "<b>Select and copy the following into excel for formatted rows:</b> </br><br><br>" + x;
-            table.innerHTML = x;
+            res.innerHTML = x;
     }
