edf2acf2d02887307f160eb76def6dd4706d11cf
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 09:25:16 -0400

    Update delimiter-regex.js

--- js/delimiter-regex.js
+++ js/delimiter-regex.js
@@ -3,10 +3,8 @@
     }
     
     const delimit = (str, res) => {
-            const x = 
+         res.innerHTML = 
          str.replace(/( )(?=(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|II|III|IV|V|JR|SR|ESQ|DE|LA|LE|VAN|DER|DEL|DELLA|E|VON|AL|BEN|BIN|DA|DAS|DEN|DI|DO|DOS|D|DU|EL|IL|L|MAC|O|SAINT|SAINTE|ST|STE|TEN|TER|V|VD|SEN|ZUM)( |\.))/ig, "900001VARHOLDER")
             .replace(/((?:[^ ]* ){1}[^ ]*) /ig, '<tr><td>'+'$1'+'<br></td></tr>')
             .replace(/900001VARHOLDER/g, " ");
-      
-            res.innerHTML = x;
     }
