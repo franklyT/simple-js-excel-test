1fcda9d78bd76f342d057f213bc6ddc0131eec02
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 19:47:22 -0400

    Update grab-metadata.js

--- js/grab-metadata.js
+++ js/grab-metadata.js
@@ -2,5 +2,5 @@ const grabMetadata = (str, res) => {
   ex1 = performance.now();
   delimit(str, res);
   ex2 = performance.now();
-  document.getElementById("metadata").innerHTML = ex2 - ex1 + ' ms to execute.' + (document.getElementsByTagName("TR").length+1) + ' names processed.';
+  document.getElementById("metadata").innerHTML = ex2 - ex1 + ' ms to execute.' + '     ' + (document.getElementsByTagName("TR").length+1) + ' names processed.';
 }
