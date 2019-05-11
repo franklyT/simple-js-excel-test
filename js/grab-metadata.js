4cfcb42f2cf04516eaebcbcde111257291f0a2a6
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 20:08:39 -0400

    Update grab-metadata.js

--- js/grab-metadata.js
+++ js/grab-metadata.js
@@ -2,9 +2,9 @@ const grabMetadata = (str, res) => {
   const ex1 = performance.now();
   delimit(str, res);
   const ex2 = performance.now();
-  const md = document.getElementById("metadata").innerHTML
-  if(md.length > 0) {
-      document.getElementById("metadata").innerHTML = 
+  const md = document.getElementById("metadata")
+  if(str.innerHTML.length > 0) {
+      md.innerHTML = 
            '~' +
             Math.round((ex2 - ex1)*1000 / 1000) + 
            ' ms to execute.' + 
