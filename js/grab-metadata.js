0b1cd2a457b73e4a330527bc9244015d387d9921
maldici <48483484+maldici@users.noreply.github.com>
2019-05-10 20:38:13 -0400

    Update grab-metadata.js

--- js/grab-metadata.js
+++ js/grab-metadata.js
@@ -1,11 +1,14 @@
 const grabMetadata = () => {
-  document.getElementById('export').style='display: block';
+  const exp = document.getElementById('export');
   const str = String(document.getElementById('string').value);
   const res = document.getElementById('results')
   const md = document.getElementById("metadata");
   const ex1 = performance.now();
   delimit(str, res);
   const ex2 = performance.now();
+  
+  exp.style='display: block';
+  
   if(res.innerHTML.length > 0) {
       md.innerHTML = 
            '~' +
